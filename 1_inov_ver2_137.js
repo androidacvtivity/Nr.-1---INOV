b83044ba3806fad2091f5c444e58ec8fd75be4f2
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2023-03-27 15:58:46 +0300

    Fixed  some bugs.

--- 1_inov_ver2_137.js
+++ 1_inov_ver2_137.js
@@ -662,6 +662,24 @@ webform.validators.inov1 = function (v, allowOverpass) {
     }
     //End--48-03901
 
+
+    //Start--48-03901
+
+    if ((!values.CAPITOL1_R111_C1 == false  || !values.CAPITOL1_R112_C1 !== false || !values.CAPITOL1_R113_C1 == false) &&
+        (values.CAPITOL1_R114_C1 !== '1.1.1' && values.CAPITOL1_R114_C1 !== '1.1.2' && values.CAPITOL1_R114_C1 !== '1.1.3')
+            && !(values.CAPITOL1_R111_C1 == false && values.CAPITOL1_R112_C1 == false && values.CAPITOL1_R113_C1 ==  false && values.CAPITOL1_R114_C1 == "")
+        ) {
+        webform.errors.push({
+            'fieldName': 'CAPITOL1_R114_C1',
+            'index': 0,
+            'msg': Drupal.t('Cod eroare: 48-03902 Trebuie de indicat din Cap.1.1 numărul rândului 1.1.1 sau 1.1.2 sau 1.1.3')
+        });
+    }
+    //End--48-03901
+
+
+
+
     //Start  48-004 - 48-006
 
     // Error codes for Capitol2 Rind2.3.3, 2.3.4, 2.3.5
