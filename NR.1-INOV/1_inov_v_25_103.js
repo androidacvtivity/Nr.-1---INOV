d87d0f753477512790d3292e0205e16803ce7126
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-03-24 17:38:08 +0200

    n-n

--- NR.1-INOV/1_inov_v_25_103.js
+++ NR.1-INOV/1_inov_v_25_103.js
@@ -116,39 +116,33 @@ function watchLiveValidation_48_008() {
 
 
 //---------------------------------------------
+//Analyze the html and js files
+//Modify these two functions - when the condition is - true - do not hide anything but make the variables read only
+//Modifica aceste doua functii -  cand  conditia este - true - nu ascunde nimic dar variabilile sa fie read only 
+
 function check_111_1129(values) {
     jQuery('input[type=checkbox]').change(function () {
         if (
 
-            ((jQuery('#CAPITOL1_R111_C2').is(':checked') && jQuery('#CAPITOL1_R112_C2').is(':checked')) &&
-                (jQuery('#CAPITOL1_R151_C2').is(':checked') && jQuery('#CAPITOL1_R152_C2').is(':checked') &&
-                    jQuery('#CAPITOL1_R153_C2').is(':checked') && jQuery('#CAPITOL1_R154_C2').is(':checked') &&
-                    jQuery('#CAPITOL1_R155_C2').is(':checked') && jQuery('#CAPITOL1_R156_C2').is(':checked') &&
-                    jQuery('#CAPITOL1_R157_C2').is(':checked')) &&
-                (
-                    jQuery('#CAPITOL1_R172_C2').is(':checked') && jQuery('#CAPITOL1_R173_C2').is(':checked') &&
-                    jQuery('#CAPITOL1_R174_C2').is(':checked') && jQuery('#CAPITOL1_R175_C2').is(':checked') &&
-                    jQuery('#CAPITOL1_R176_C2').is(':checked') && jQuery('#CAPITOL1_R177_C2').is(':checked')
-                ))
+          
 
 
-            && (
+             (
 
-                jQuery('#CAPITOL1_R1101_C1').is(':checked') || jQuery('#CAPITOL1_R1102_C1').is(':checked') ||
-                jQuery('#CAPITOL1_R1103_C1').is(':checked')
+                jQuery('#CAPITOL1_R1111_C2').is(':checked') && jQuery('#CAPITOL1_R1112_C2').is(':checked') 
+              
             )
 
 
         ) {
             // Hide 1.6 and move to 1.7
-            jQuery('#CAPITOL1_R111H1, #CAPITOL1_R111H2, #CAPITOL1_R111H3, #CAPITOL1_R111H4, #CAPITOL1_R1111, #CAPITOL1_R1112, #CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').hide();
+            jQuery('#CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').hide();
 
             // Clear input values
 
 
             var inputIDs = [
-                "CAPITOL1_R1111_C1", "CAPITOL1_R1111_C2",
-                "CAPITOL1_R1112_C1", "CAPITOL1_R1112_C2",
+                
                 "CAPITOL1_R1113_C1", "CAPITOL1_R1113_C2",
                 "CAPITOL1_R1121_C1", "CAPITOL1_R1121_C2", "CAPITOL1_R1121_C3",
                 "CAPITOL1_R1122_C1", "CAPITOL1_R1122_C2", "CAPITOL1_R1122_C3",
@@ -175,7 +169,7 @@ function check_111_1129(values) {
 
         } else {
             // Show elements of 1.6
-            jQuery('#CAPITOL1_R111H1, #CAPITOL1_R111H2, #CAPITOL1_R111H3, #CAPITOL1_R111H4, #CAPITOL1_R1111, #CAPITOL1_R1112, #CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').show();
+            jQuery('#CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').show();
         }
     });
 }
@@ -183,26 +177,17 @@ function check_111_1129(values) {
 
 function toggle111_1129(values) {
     if (
-        values.CAPITOL1_R111_C2 == '1' && values.CAPITOL1_R112_C2 == '1' &&
-        values.CAPITOL1_R151_C2 == '1' && values.CAPITOL1_R152_C2 == '1' &&
-        values.CAPITOL1_R153_C2 == '1' && values.CAPITOL1_R154_C2 == '1' &&
-        values.CAPITOL1_R155_C2 == '1' && values.CAPITOL1_R156_C2 == '1' &&
-        values.CAPITOL1_R157_C2 == '1' &&
-        values.CAPITOL1_R171_C2 == '1' && values.CAPITOL1_R172_C2 == '1' &&
-        values.CAPITOL1_R173_C2 == '1' && values.CAPITOL1_R174_C2 == '1' &&
-        values.CAPITOL1_R175_C2 == '1' && values.CAPITOL1_R176_C2 == '1' &&
-        values.CAPITOL1_R177_C2 == '1' &&
+     
         (
-            values.CAPITOL1_R1101_C1 == '1' ||
-            values.CAPITOL1_R1102_C1 == '1' ||
-            values.CAPITOL1_R1103_C1 == '1'
+            values.CAPITOL1_R1111_C2 == '1' &&
+            values.CAPITOL1_R1112_C2 == '1' 
+            
         )
     ) {
-        jQuery('#CAPITOL1_R111H1, #CAPITOL1_R111H2, #CAPITOL1_R111H3, #CAPITOL1_R111H4, #CAPITOL1_R1111, #CAPITOL1_R1112, #CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').hide();
+        jQuery('#CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').hide();
 
         var inputIDs = [
-            "CAPITOL1_R1111_C1", "CAPITOL1_R1111_C2",
-            "CAPITOL1_R1112_C1", "CAPITOL1_R1112_C2",
+           
             "CAPITOL1_R1113_C1", "CAPITOL1_R1113_C2",
             "CAPITOL1_R1121_C1", "CAPITOL1_R1121_C2", "CAPITOL1_R1121_C3",
             "CAPITOL1_R1122_C1", "CAPITOL1_R1122_C2", "CAPITOL1_R1122_C3",
@@ -226,7 +211,7 @@ function toggle111_1129(values) {
             }
         });
     } else {
-        jQuery('#CAPITOL1_R111H1, #CAPITOL1_R111H2, #CAPITOL1_R111H3, #CAPITOL1_R111H4, #CAPITOL1_R1111, #CAPITOL1_R1112, #CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').show();
+        jQuery('#CAPITOL1_R1113, #CAPITOL1_R111H5, #CAPITOL1_R112H1, #CAPITOL1_R112H2, #CAPITOL1_R112H3, #CAPITOL1_R112H4, #CAPITOL1_R112H5, #CAPITOL1_R112H6, #CAPITOL1_R112H7, #CAPITOL1_R112H8, #CAPITOL1_R1121, #CAPITOL1_R1122, #CAPITOL1_R1123, #CAPITOL1_R1124, #CAPITOL1_R1125, #CAPITOL1_R1126, #CAPITOL1_R1127, #CAPITOL1_R1128, #CAPITOL1_R1129').show();
     }
 }
 
