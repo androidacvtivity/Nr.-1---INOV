b47c3ddd2cb556d60eb6dd1f74c4aa47fe906838
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-04-02 11:10:56 +0300

    nn

--- NR.1-INOV/inov_90_103.js
+++ NR.1-INOV/inov_90_103.js
@@ -1205,47 +1205,43 @@ webform.validators.inov1 = function (v, allowOverpass) {
 //-------------------------------------------------------------------------------------------------
 
 function validate48_0017() {
-    const r111_selected = jQuery('#CAPITOL1_R111_C1').is(':checked') || jQuery('#CAPITOL1_R111_C2').is(':checked');
-    const r112_selected = jQuery('#CAPITOL1_R112_C1').is(':checked') || jQuery('#CAPITOL1_R112_C2').is(':checked');
+    const r111_checked = jQuery('#CAPITOL1_R111_C1').is(':checked') || jQuery('#CAPITOL1_R111_C2').is(':checked');
+    const r112_checked = jQuery('#CAPITOL1_R112_C1').is(':checked') || jQuery('#CAPITOL1_R112_C2').is(':checked');
 
-    const r15_complete = [
+    const all15_checked = [
         '#CAPITOL1_R151_C1', '#CAPITOL1_R151_C2',
         '#CAPITOL1_R152_C1', '#CAPITOL1_R152_C2',
         '#CAPITOL1_R153_C1', '#CAPITOL1_R153_C2',
         '#CAPITOL1_R154_C1', '#CAPITOL1_R154_C2',
         '#CAPITOL1_R155_C1', '#CAPITOL1_R155_C2',
         '#CAPITOL1_R156_C1', '#CAPITOL1_R156_C2',
-        '#CAPITOL1_R157_C1', '#CAPITOL1_R157_C2',
-    ].every(id => jQuery(id).is(':checked'));
+        '#CAPITOL1_R157_C1', '#CAPITOL1_R157_C2'
+    ].every(sel => jQuery(sel).is(':checked'));
 
-    const r17_da = [
+    const atLeastOne17_DA = [
         '#CAPITOL1_R171_C1',
         '#CAPITOL1_R174_C1',
         '#CAPITOL1_R175_C1',
         '#CAPITOL1_R176_C1',
-        '#CAPITOL1_R177_C1',
-    ].some(id => jQuery(id).is(':checked'));
+        '#CAPITOL1_R177_C1'
+    ].some(sel => jQuery(sel).is(':checked'));
 
-    const r110_bifate = [
-        '#CAPITOL1_R1101_C1',
-        '#CAPITOL1_R1102_C1',
-        '#CAPITOL1_R1103_C1'
-    ].some(id => jQuery(id).is(':checked'));
+ 
 
-    const r111_bifate = [
+    const all111_unchecked = [
         '#CAPITOL1_R1111_C1', '#CAPITOL1_R1111_C2',
         '#CAPITOL1_R1112_C1', '#CAPITOL1_R1112_C2',
         '#CAPITOL1_R1113_C1', '#CAPITOL1_R1113_C2'
-    ].some(id => jQuery(id).is(':checked'));
+    ].every(sel => !jQuery(sel).is(':checked'));
 
-    if (r111_selected && r112_selected && r15_complete && r17_da && !r110_bifate && !r111_bifate) {
+    if ((r111_checked && r112_checked && all15_checked && atLeastOne17_DA ) && all111_unchecked) {
         webform.errors.push({
             fieldName: 'CAPITOL1_R1111_C1',
             weight: 17,
             msg: concatMessage(
                 '48-0017',
-                'Întrebarea 1.11 – Cooperare',
-                Drupal.t('Cod eroare: 48-0017. Completati Cap.1 Rindurile 1.11.')
+                'Întrebarea 1.11 – Cooperare cu alte întreprinderi sau organizații',
+                Drupal.t('Cod eroare: 48-0017. Completati Cap.1 Rindurile 1.11 – trebuie selectat cel puțin un rând.')
             )
         });
     }
