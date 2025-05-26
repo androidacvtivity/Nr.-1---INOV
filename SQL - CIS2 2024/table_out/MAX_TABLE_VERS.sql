c807586efbe9c7fef05dd00bf6b6573441015d13
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-05-26 08:11:48 +0300

    SQL - 2024

--- SQL - CIS2 2024/table_out/MAX_TABLE_VERS.sql
+++ SQL - CIS2 2024/table_out/MAX_TABLE_VERS.sql
@@ -1,1 +1,13 @@
 ﻿
+
+
+            SELECT 
+            TABELE,
+            MAX(TABELE_VERS) TABELE_VERS
+            
+             FROM  CIS2.MD_TABLES
+             
+             WHERE 
+             FORM = 48
+             GROUP BY 
+             TABELE
